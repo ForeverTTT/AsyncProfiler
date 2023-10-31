48c200ee6125392aa1ddfa8d685e605d60b4a0a1
ge95nej <ge95nej@mytum.de>
2023-10-31 15:52:39 +0100

    solved bugs

--- src/main/java/org/example/PerformanceExample.java
+++ src/main/java/org/example/PerformanceExample.java
@@ -1,2 +1,23 @@
-package org.example;public class PerformanceExample {
+package org.example;
+
+public class PerformanceExample {
+    public static void main(String[] args) {
+        long result = performHeavyComputation(1000000000);
+       // long result = performOptimizedComputation(100000000);
+        System.out.println("Result: " + result);
+    }
+
+    public static long performHeavyComputation(int n) {
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static long performOptimizedComputation(int n) {
+        return (long) n * (n - 1) / 2;
+    }
 }
+
+// other possibilities: Binary Exponentiation vs iteration, merge sort vs bubble sort
